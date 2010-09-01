f176a3b11436aac49bc7147a03e6e389936fc94a
Sheldon Dueck <sheldon.dueck@gmail.com>
2010-09-01 23:52:33 +0800

    require event.rb before user.rb

--- lib/eventbright.rb
+++ lib/eventbright.rb
@@ -2,4 +2,5 @@ libdir = File.dirname(__FILE__)
 $LOAD_PATH.unshift(libdir) unless $LOAD_PATH.include?(libdir)
 require File.join(libdir, 'eventbright', 'main.rb')
 Dir.glob(File.join(libdir, 'eventbright', '*.rb')).each {|f| require f }
+require File.join(libdir, 'eventbright', 'api_objects', 'event.rb')
 Dir.glob(File.join(libdir, 'eventbright', 'api_objects', '*.rb')).each {|f| require f }
